7f954abcea234aa4a9fe313c790f13be4aac0e16
yangshengcheng <yangshengcheng@gmail.com>
2011-08-18 15:25:38 +0800

    query weblogic mbean data depend on bhm_wls_mbean.xml

---
+++ policy/bhm_weblogic/UNIX/AIX/bhm_weblogic_metric.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/sh
+
+######################################
+#desc: for AIX only
+#author: yangshengcheng@gzcss.net
+#create : 2011.7.28
+#version : 20110728
+#modified: create
+#####################################
+
+
+if [ -z "$JAVA_HOME" ]; then
+        #if JAVA_HOME variable is empty ,you should set it below
+
+        JAVA_HOME=
+        export JAVA_HOME
+fi
+
+# native jar,if use "-c native" you must set this follow jar dir 
+wlclient=/var/opt/OV/bin/instrumentation/wlclient.jar
+wljmxclient=/var/opt/OV/bin/instrumentation/wljmxclient.jar
+
+CLASSPATH=.:$CLASSPATH:$wlclient:$wljmxclient:/var/opt/OV/bin/instrumentation/jdom.jar:/var/opt/OV/bin/instrumentation/commons-cli-1.2.jar
+export CLASSPATH
+
+EXECUTEDIR=/var/opt/OV/bin/instrumentation/
+
+CLASSFILE=/var/opt/OV/bin/instrumentation/wlsConnectRemote.class
+
+cd $EXECUTEDIR
+
+if [ ! -e $CLASSFILE ]; then
+        $JAVA_HOME/bin/javac $EXECUTEDIR/wlsConnectRemote.java
+
+fi
+
+$JAVA_HOME/bin/java -cp "$CLASSPATH"  wlsConnectRemote $@
---
+++ policy/bhm_weblogic/Windows/bhm_weblogic_metric.bat
@@ -0,0 +1,24 @@
+@echo  off
+
+rem set bhm_lib ,add jdom and apache commons jar
+rem set bhm_lib ,add jdom ,apache commons jar,wlclient.jar,wljmxclient.jar
+
+SET wlclient=%OvDataDir%\bin\instrumentation\wlclient.jar
+SET wljmxclient=%OvDataDir%\bin\instrumentation\wljmxclient.jar
+
+SET CLASSPATH=%CLASSPATH%;%wlclient%;%wljmxclient%;%OvDataDir%\bin\instrumentation\jdom.jar;%OvDataDir%\bin\instrumentation\commons-cli-1.2.jar;
+SET EXECUTEDIR=%OvDataDir%\bin\instrumentation
+
+chdir /D %EXECUTEDIR%
+
+SET CLASSFILE="%EXECUTEDIR%\wlsConnectRemote.class"
+
+rem recompile first time
+
+if NOT EXIST %CLASSFILE% (
+	"%JAVA_HOME%\bin\javac" wlsConnectRemote.java
+)
+
+"%JAVA_HOME%\bin\java.exe" -Xms256M -Xmx512M -cp "%CLASSPATH%" wlsConnectRemote %*
+
+:END
